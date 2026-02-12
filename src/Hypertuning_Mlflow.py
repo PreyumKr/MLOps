5853214c404c6f98e7bf88c6cd4cbd53d6f2295b
preyumkr <preyumkrsingh@gmail.com>
2026-02-12 22:34:15 +0530

    MLFlow learning complete

---
+++ src/Hypertuning_Mlflow.py
@@ -0,0 +1,69 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+import mlflow
+
+import dagshub
+dagshub.init(repo_owner='PreyumKr', repo_name='MLOps', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/PreyumKr/MLOps.mlflow")
+
+# Load the dataset for breast cancer
+data= load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target, name='target')
+
+# Lets split the data into training and testing sets 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+
+# Creating the model
+rf = RandomForestClassifier(random_state=42)
+
+# Creating the parameter grid for cross validation
+param_grid = {
+    'n_estimators': [10, 50, 100],
+    'max_depth': [None, 10, 20, 30]
+}
+
+# Applying GridSearchCV for cross validation
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
+
+mlflow.set_experiment("MLFlow_Demo_Nested_Runs")
+
+with mlflow.start_run() as parent:
+    grid_search.fit(X_train, y_train)
+
+    # Logging the child runs
+    for i in range(len(grid_search.cv_results_['params'])):
+
+        with mlflow.start_run(nested=True) as child:
+            mlflow.log_params(grid_search.cv_results_['params'][i])
+            mlflow.log_metric('mean_test_score', grid_search.cv_results_['mean_test_score'][i])
+
+    
+    # Recording the best parameters and the best score
+    best_params = grid_search.best_params_
+    best_score = grid_search.best_score_
+
+    mlflow.log_params(best_params)
+    mlflow.log_metric("accuracy", best_score)
+
+    train_df = X_train.copy()
+    train_df['target'] = y_train
+
+    test_df = X_test.copy()
+    test_df['target'] = y_test
+
+    train_df = mlflow.data.from_pandas(train_df)
+    test_df = mlflow.data.from_pandas(test_df)
+    mlflow.log_input(train_df, 'training_data')
+    mlflow.log_input(test_df, 'testing_data')
+
+    mlflow.sklearn.log_model(grid_search.best_estimator_, "RandomForestModel", serialization_format="cloudpickle")
+
+    mlflow.set_tag('Author', 'Preyum Kumar')
+
+    print("Best Parameters: ", best_params)
+    print("Best Score: ", best_score)
